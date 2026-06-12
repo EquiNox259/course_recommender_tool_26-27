642b59311932f1dd52f83b9354ea791c551f0b9a
Aryan Chauhan <linkaryan3@gmail.com>
2026-06-12 12:59:01 +0530

    Removing hard-coded dependency for semesters

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ COURSES_HISTORY_PATH = os.path.join(DATASET_DIR, "Student_data.csv")
 RUNNING_COURSES_PATH = os.path.join(DATASET_DIR, "courses_spring_2025-2026.csv")
 PREREQ_PATH = os.path.join(DATASET_DIR, "Prerequisite_new.xlsx")
 CORE_COURSES_PATH = os.path.join(DATASET_DIR, "ASC_Core_Courses.csv")
+SEMESTER = 'Spring'
 
 # Model artifacts
 MODEL_DIR = os.path.join(BASE_DIR, "model")
